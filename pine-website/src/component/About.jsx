296ce317efca567c9541a0693749002eb682b3b9
Pine <pinetcht@gmail.com>
2024-04-05 18:03:45 +0200

    fixed the issues! (className not class; # for IDs; imported pic from Asset)

--- pine-website/src/component/About.jsx
+++ pine-website/src/component/About.jsx
@@ -1,18 +1,19 @@
 import "../style/About.css";
+import pinePic from "../assets/pine_cph.jpg";
 
 const About = () => {
   return (
     <>
-      <h1 style="color:#e7ebd4">Pine's Personal Website</h1>
-      <img src="pine_cph.jpg" width="100" alt="Pine in Copenhagen" />
+      <h1 style={{ color: '#e7ebd4' }}>Pine's Personal Website</h1>
+      <img src={pinePic} width="100" alt="Pine in Copenhagen" />
 
       <h3>Facts about Pine</h3>
       <ul>
         <li id="name">Full Name: Teechutha Netcharussaeng</li>
-        <li class="places">Hometown: Bangkok, Thailand</li>
-        <li class="places">College: Pomona College</li>
+        <li className="places">Hometown: Bangkok, Thailand</li>
+        <li className="places">College: Pomona College</li>
         <li>
-          Fun fact: I was named after
+          Fun fact: I was named after&nbsp; 
           <a href="https://en.wikipedia.org/wiki/Pine_(email_client)">
             an email reader!</a>
         </li>
